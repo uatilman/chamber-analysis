ad95fadf218a5c8a5a526c3cd54ecb3b8f69e40f
uatilman <uatilman@gmail.com>
2018-08-02 15:16:41 +0300

    rename javascript variables. commit before drop to send home task

--- src/main/resources/static/getChambers.js
+++ src/main/resources/static/getChambers.js
@@ -1,13 +1,13 @@
 const size = 20;
 const order = "ASC";
 const orderBy = "name";
-const pageNumber = 0;
-let chamberBody;
+let pageNumber = 0;
+let rowTemplate;
 
 $(document).ready(
     function () {
         loadChambers();
-        chamberBody = $(`.click-row`).clone();
+        rowTemplate = $(`.click-row`).clone();
         $('.click-row').remove();
         $(window).scroll(function () {
             if ($(window).scrollTop() === $(document).height() - $(window).height()) {
@@ -17,26 +17,33 @@ $(document).ready(
     }
 );
 
-
 function renderingChambers(chambers) {
     chambers.forEach(function (chamber) {
-        var test = $(chamberBody).clone()
+        let row = $(rowTemplate).clone()
             .find(".id-column").html(chamber["id"]).end()
             .find(".name-column").html(chamber["name"]).end()
             .find(".address-column").html(chamber["address"]).end();
-        $(test).appendTo(".chamber-table");
+        $(row.attr("data-href", "chambers?id=" + chamber["id"]));
+
+        $(row).appendTo(".chamber-table");
 
     });
 }
 
-/*function setRowClickListener() {
+function setRowClickListener() {
     $(".click-row").click(function () {
         window.location = $(this).data("href");
         return false;
     });
-}*/
+}
+
 function loadChambers() {
-    var data = "pageNumber=" + pageNumber + "&" + "order=" + order + "&" + "orderBy=" + orderBy + "&" + "size=" + size;
+
+    let data =
+        "pageNumber=" + pageNumber + "&" +
+        "order=" + order + "&" +
+        "orderBy=" + orderBy + "&" +
+        "size=" + size;
 
     $.ajax({
         url: '/rest/getChambers',
@@ -47,7 +54,7 @@ function loadChambers() {
             function (chambersResponsive) {
                 if (chambersResponsive !== 0) {
                     renderingChambers(chambersResponsive);
-                    // setRowClickListener();
+                    setRowClickListener();
                     pageNumber++;
                 }
             }
