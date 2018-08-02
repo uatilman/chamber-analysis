f51cfb26ce4fb8133b0b88ab21bd6d966851ec91
uatilman <uatilman@gmail.com>
2018-08-02 17:45:42 +0300

    added change language script

--- src/main/java/ru/tilman/chamberanalysis/ChamberAnalysisApplication.java
+++ src/main/java/ru/tilman/chamberanalysis/ChamberAnalysisApplication.java
@@ -1,13 +1,20 @@
 package ru.tilman.chamberanalysis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 
 @SpringBootApplication
-public class ChamberAnalysisApplication {
+public class ChamberAnalysisApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ChamberAnalysisApplication.class, args);
@@ -21,5 +28,23 @@ public class ChamberAnalysisApplication {
         return messageSource;
     }
 
+    @Bean("localeChangeInterceptor")
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean("localeResolver")
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localResolver = new CookieLocaleResolver();
+        localResolver.setCookieName("locale");
+        return localResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 
 }
