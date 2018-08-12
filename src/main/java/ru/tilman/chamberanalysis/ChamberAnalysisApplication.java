f519411b388cd8ea42312a898de91ea46916298d
uatilman <uatilman@gmail.com>
2018-08-12 17:09:17 +0300

    drop test maven files neen to check heroku deploy

--- src/main/java/ru/tilman/chamberanalysis/ChamberAnalysisApplication.java
+++ src/main/java/ru/tilman/chamberanalysis/ChamberAnalysisApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
-
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class ChamberAnalysisApplication implements WebMvcConfigurer {
