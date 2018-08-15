f21131176aa9efdce9e01ef40ed550a8ea3321bf
uatilman <uatilman@gmail.com>
2018-08-15 15:12:35 +0300

    add spring cloud config module and implement into client

--- chambers-enterprise/src/main/java/ru/tilman/chambers/enterprise/ChamberAnalysisApplication.java
+++ chambers-enterprise/src/main/java/ru/tilman/chambers/enterprise/ChamberAnalysisApplication.java
@@ -1,6 +1,7 @@
 package ru.tilman.chambers.enterprise;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+//@EnableAutoConfiguration
 public class ChamberAnalysisApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
